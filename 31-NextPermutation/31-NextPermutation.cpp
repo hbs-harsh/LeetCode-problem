c62862baa09c9a758d8031fd8b664fcad5726cdd
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-04 22:14:04 +0530

    Create file

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,28 @@
+// Last updated: 5/4/2025, 10:14:05 PM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int ind=-1;
+        int n=nums.size();
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind ==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+ind+1,nums.end());
+        
+    }
+};
