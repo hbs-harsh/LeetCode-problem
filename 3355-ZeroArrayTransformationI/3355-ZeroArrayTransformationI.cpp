1b6ef5501db55ac31bbe3eabc9fed3a001aff8b9
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-22 22:40:58 +0530

    File updated at 5/22/2025, 10:40:57 PM

--- 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.cpp
+++ 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.cpp
@@ -1,14 +1,14 @@
-// Last updated: 5/21/2025, 12:32:42 AM
+// Last updated: 5/22/2025, 10:40:57 PM
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
     vector<int> count(n + 1, 0);
         for(auto& q:queries){
-            int l=q[0];
-            int r=q[1];
-              count[l] += 1;
-        if (r + 1 < n) count[r + 1] -= 1;
+            int start=q[0];
+            int end=q[1];
+              count[start] += 1;
+        if (end + 1 < n) count[end + 1] -= 1;
         }
 
              for (int i = 1; i < n; ++i) {
