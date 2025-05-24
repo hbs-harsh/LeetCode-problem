e1edb99f463c08cc12cb162cb316b9345a7fd961
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-25 00:40:50 +0530

    File updated at 5/25/2025, 12:40:49 AM

--- 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
+++ 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
@@ -1,16 +1,21 @@
-// Last updated: 5/25/2025, 12:32:42 AM
+// Last updated: 5/25/2025, 12:40:49 AM
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
 
-        vector<int>ans;
+        // lower_bound: first position >= target
+        // upper_bound: first position > target
+        auto low = lower_bound(nums.begin(), nums.end(), target);
+        auto up = upper_bound(nums.begin(), nums.end(), target);
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                ans.push_back(i);
-            }
+        vector<int> ans;
+
+        // Fill all indices from low to up - 1
+        for (auto it = low; it != up; ++it) {
+            ans.push_back(it - nums.begin());
         }
+
         return ans;
     }
 };
