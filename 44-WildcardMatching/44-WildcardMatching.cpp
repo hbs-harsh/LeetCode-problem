97f46839c2cbb18d2def212ba378bc30cdc50430
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-26 01:28:52 +0530

    File updated at 5/26/2025, 1:28:52 AM

--- 44-WildcardMatching/44-WildcardMatching.cpp
+++ 44-WildcardMatching/44-WildcardMatching.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/26/2025, 1:24:11 AM
+// Last updated: 5/26/2025, 1:28:52 AM
 class Solution {
 public:
     bool isMatch(string s, string p) {
@@ -6,29 +6,32 @@ public:
         int m = p.size();
 
      
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        
 
-        dp[0][0] = true;
+        vector<int>prev(m+1,false),curr(m+1,false);
+        prev[0] = true;
 
         for (int j = 1; j <= m; j++) {
             if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 1]; 
+               prev[j] = prev[j - 1]; 
             }
         }
 
         for (int i = 1; i <= n; i++) {
+            curr[0]=false;
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
+                     curr[j] = prev[j - 1];
                 } else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                   curr[j] = prev[j] || curr[j - 1];
                    
                 } else {
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
