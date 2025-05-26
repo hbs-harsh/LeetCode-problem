dd9c68a95433f9eec34f24e59aa61f4890dad47f
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-27 00:15:29 +0530

    File updated at 5/27/2025, 12:15:29 AM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,21 +1,17 @@
-// Last updated: 5/27/2025, 12:13:36 AM
+// Last updated: 5/27/2025, 12:15:29 AM
 class Solution {
 public:
-int solve(int i,int n, vector<int>&dp){
-    if(i==n){
-        return 1;
-    }
-    if(i>n){
-        return 0;
-    }
-    if(dp[i]!=-1){
-        return dp[i];
-    }
 
-    return dp[i]=solve(i+1,n,dp)+solve(i+2,n,dp);
-}
     int climbStairs(int n) {
-        vector<int>dp(n,-1);
-        return solve(0,n,dp);
+        vector<int>dp(n+1,0);
+        
+
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
     }
 };
