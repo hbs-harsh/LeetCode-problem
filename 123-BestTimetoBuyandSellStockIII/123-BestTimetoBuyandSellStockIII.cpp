770b968dcf903fd79fe10baec67a18eea1c90466
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-29 02:10:14 +0530

    File updated at 5/29/2025, 2:10:13 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
@@ -1,25 +1,26 @@
-// Last updated: 5/29/2025, 1:57:33 AM
+// Last updated: 5/29/2025, 2:10:13 AM
 class Solution {
 public:
-int solve(int ind,int buy,vector<int>&prices,int n,int limit,vector<vector<vector<int>>>&dp){
 
-    if(ind == n || limit==0){
-        return 0;
-    }
-    int profit=0;
-    if(dp[ind][buy][limit]!=-1) return dp[ind][buy][limit];
-    if(buy){
-        profit = max(-prices[ind]+solve(ind+1,0,prices,n,limit,dp),solve(ind+1,1,prices,n,limit,dp));
-    }else{//sell
-    profit = max(prices[ind]+solve(ind+1,1,prices,n,limit-1,dp),solve(ind+1,0,prices,n,limit,dp));
-    }
-    return  dp[ind][buy][limit]=profit;
-
-}
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
 
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(0,1,prices,n,2,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+     
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int limit=1;limit<=2;limit++){
+                     if(buy){
+                        dp[ind][buy][limit]= max(-prices[ind]+dp[ind+1][0][limit],dp[ind+1][1][limit]);
+                        }else{//sell
+                       dp[ind][buy][limit] = max(prices[ind]+dp[ind+1][1][limit-1],dp[ind+1][0][limit]);
+                        }
+                    
+                }
+            }
+        }
+        return dp[0][1][2];
+
     }
 };
