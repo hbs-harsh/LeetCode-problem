57ab5f26799eeddd98608aad2dd60c96d7b909f0
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-29 01:16:54 +0530

    File updated at 5/29/2025, 1:16:54 AM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.cpp
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.cpp
@@ -1,26 +1,19 @@
-// Last updated: 5/29/2025, 12:44:15 AM
+// Last updated: 5/29/2025, 1:16:54 AM
 class Solution {
 public:
-
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-      vector<vector<int>>dp(n+1,vector<int>(2,0));
-        int profit=0;
-       
-       dp[n][0]=dp[n][1]=0;
+        int n = prices.size();
+        vector<int> ahead(2, 0), curr(2, 0); 
 
-       for(int i=n-1;i>=0;i--){
-        for(int buy=0;buy<=1;buy++){
-           
-            if(buy){
-        profit = max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
-    }
-    else{
-        profit=max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
-    }
-    dp[i][buy]=profit;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy)
+                    curr[buy] = max(-prices[i] + ahead[0], ahead[1]);
+                else
+                    curr[buy] = max(prices[i] + ahead[1], ahead[0]);
+            }
+            ahead = curr; 
         }
-       }
-       return profit;
+        return ahead[1]; 
     }
 };
