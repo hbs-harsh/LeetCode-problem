7a48f214f7652b14e23e2629c92b66ff640c1e1a
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-29 03:21:19 +0530

    Create 90-SubsetsII

---
+++ 90-SubsetsII/90-SubsetsII.cpp
@@ -0,0 +1,21 @@
+// Last updated: 5/29/2025, 3:21:19 AM
+class Solution {
+public:
+void findSubsequence(int index,vector<int>&ds,vector<vector<int>>&ans,vector<int>& nums){
+    ans.push_back(ds);
+ for(int i=index;i<nums.size();i++){
+    if(i!=index&&nums[i]==nums[i-1]) continue;
+    ds.push_back(nums[i]);
+    findSubsequence(i+1,ds,ans,nums);
+    ds.pop_back();
+}
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        findSubsequence(0,ds,ans,nums);
+      
+        return ans;
+    }
+};
