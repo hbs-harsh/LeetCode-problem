e27afad7babfe9aacc8e4621bb7e70fd50d974ef
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-31 00:11:02 +0530

    File updated at 5/31/2025, 12:11:02 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
@@ -1,25 +1,33 @@
-// Last updated: 5/31/2025, 12:10:10 AM
+// Last updated: 5/31/2025, 12:11:02 AM
 class Solution {
 public:
-int solve(int ind,int buy,vector<int>&prices,int n,int limit,vector<vector<vector<int>>>&dp){
+int solve(int ind,int buy,vector<int>& prices,int n,int k, vector<vector<vector<int>>>&dp){
+int profit=0;
+if(ind==n){
+    return 0;
+}
+if(k<1){
+    return 0;
+}
+if(dp[ind][buy][k]!=-1){
+    return dp[ind][buy][k];
+}
 
-    if(ind == n || limit==0){
-        return 0;
-    }
-    int profit=0;
-    if(dp[ind][buy][limit]!=-1) return dp[ind][buy][limit];
     if(buy){
-        profit = max(-prices[ind]+solve(ind+1,0,prices,n,limit,dp),solve(ind+1,1,prices,n,limit,dp));
-    }else{//sell
-    profit = max(prices[ind]+solve(ind+1,1,prices,n,limit-1,dp),solve(ind+1,0,prices,n,limit,dp));
+        profit= max(-prices[ind]+solve(ind+1,0,prices,n,k,dp),0+solve(ind+1,1,prices,n,k,dp));
     }
-    return  dp[ind][buy][limit]=profit;
+    else{
+        profit=max(prices[ind]+solve(ind+1,1,prices,n,k-1,dp),0+solve(ind+1,0,prices,n,k,dp));
+    }
+    return dp[ind][buy][k]=profit;
 
 }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(0,1,prices,n,2,dp);
+        
+       int k=2;
+         return solve(0,1,prices,n,k,dp);
+       
     }
 };
