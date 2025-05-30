97dc6821727c73a9331f4111720889f0ff6b945c
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-05-31 00:17:43 +0530

    File updated at 5/31/2025, 12:17:42 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
@@ -1,27 +1,28 @@
-// Last updated: 5/31/2025, 12:15:27 AM
+// Last updated: 5/31/2025, 12:17:42 AM
 class Solution {
 public:
 
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-
         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        int profit=0;
+        
+      
+       int profit=0;
+       for(int ind=n-1;ind>=0;ind--){
+        for(int buy=0;buy<=1;buy++){
+            for(int k=1;k<=2;k++){
 
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                for(int limit=1;limit<=2;limit++){
-                     if(buy){
-                        profit = max(-prices[ind]+dp[ind+1][0][limit],dp[ind+1][1][limit]);
-                        }else{//sell
-                       profit = max(prices[ind]+dp[ind+1][1][limit-1],dp[ind+1][0][limit]);
-                        }
-                    dp[ind][buy][limit]=profit;     
-                }
+                  if(buy){
+        profit= max(-prices[ind]+dp[ind+1][0][k],0+dp[ind+1][1][k]);
+    }
+    else{
+        profit=max(prices[ind]+dp[ind+1][1][k-1],dp[ind+1][0][k]);
+         }
+         dp[ind][buy][k]=profit;
             }
+            
         }
-         
-        return profit;
-
+       }
+       return profit;
     }
 };
