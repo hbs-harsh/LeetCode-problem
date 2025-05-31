044cba4c4ea4778cc3901f3ff86f2385161baa54
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-06-01 00:33:51 +0530

    Create 309-BestTimetoBuyandSellStockwithCooldown

---
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.cpp
@@ -0,0 +1,25 @@
+// Last updated: 6/1/2025, 12:33:50 AM
+class Solution {
+public:
+int solve(int ind,int buy,vector<int>&prices,int n,vector<vector<int>>&dp){
+    int profit=0;
+    if(ind>=n){
+        return 0;
+    }
+    if(dp[ind][buy]!=-1) return dp[ind][buy];
+    if(buy){
+        profit=max(-prices[ind] + solve(ind+1,0,prices,n,dp),0+solve(ind+1,1,prices,n,dp));
+
+    }else{
+        profit=max(prices[ind]+solve(ind+2,1,prices,n,dp),solve(ind+1,0,prices,n,dp));
+    }
+    return dp[ind][buy]=profit;
+}
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+
+        return solve(0,1,prices,n,dp);
+    }
+};
