343ea28b09e9ef1f2026f92792310f8ebaadcaca
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-06-02 00:20:00 +0530

    File updated at 6/2/2025, 12:20:00 AM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
@@ -1,33 +1,25 @@
-// Last updated: 6/2/2025, 12:17:17 AM
+// Last updated: 6/2/2025, 12:20:00 AM
 class Solution {
 public:
-int longestLen(int ind,int prev_ind,vector<int>&nums,int n,vector<vector<int>>&dp){
-    if(ind==n){
-        return 0;
-    }
-if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
-    int len=0+longestLen(ind+1,prev_ind,nums,n,dp);
-    if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-        len=max(len,1+longestLen(ind+1,ind,nums,n,dp));
-    }
-    return dp[ind][prev_ind+1]=len;
 
-}
+
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+      
         
+        vector<int>next(n+1,0),curr(n+1,0);
 
         for(int ind=n-1;ind>=0;ind--){
             for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--){
-                int len =0+dp[ind+1][prev_ind+1];
+                int len =0+next[prev_ind+1];
 
                 if(prev_ind==-1||nums[ind]>nums[prev_ind]){
-                    len=max(len,1+dp[ind+1][ind+1]);
+                    len=max(len,1+next[ind+1]);
                 }
-                dp[ind][prev_ind+1]=len;
+                curr[prev_ind+1]=len;
             }
+            next=curr;
         }
-        return dp[0][-1+1];
+        return next[-1+1];
     }
 };
