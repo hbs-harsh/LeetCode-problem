385f86c66640c8fa0f69295dd157e8dc3e89f1f2
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-06-24 14:38:01 +0530

    File updated at 6/24/2025, 2:38:02 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
@@ -1,16 +1,24 @@
-// Last updated: 6/24/2025, 2:29:23 PM
+// Last updated: 6/24/2025, 2:38:02 PM
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_map<int, int> map; 
-        
-        for(int i = 0; i < n; i++) {
-            if(map.find(nums[i]) != map.end() && i - map[nums[i]] <= k) {
-                return true;
+        unordered_set<int> window;
+        int i = 0;  // left pointer
+
+        for (int j = 0; j < nums.size(); j++) {
+            if (window.find(nums[j]) != window.end()) {
+                return true;  // duplicate found in window
             }
-            map[nums[i]] = i; 
+
+            window.insert(nums[j]);
+
+            // If window size exceeds k, shrink from the left
+            if (j - i >= k) {
+                window.erase(nums[i]);
+                i++;
+            }
         }
+
         return false;
     }
 };
