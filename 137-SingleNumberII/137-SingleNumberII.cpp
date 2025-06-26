8f7fe9e391b25175fbac4daa31c03c6590f0324b
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-06-26 16:24:40 +0530

    File updated at 6/26/2025, 4:24:40 PM

--- 137-SingleNumberII/137-SingleNumberII.cpp
+++ 137-SingleNumberII/137-SingleNumberII.cpp
@@ -1,18 +1,24 @@
-// Last updated: 6/26/2025, 3:59:23 PM
+// Last updated: 6/26/2025, 4:24:40 PM
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;
+        int res=0;
+        for(int i=0;i<32;i++){
+            int cntZero=0;
+            int cntOnes=0;
 
-        for(int num:nums){
-            mp[num]++;
-        }
-        int ans=0;
-        for(auto&it:mp){
-            if(it.second==1){
-                ans=it.first;
+            for(int val:nums){
+                if((val&(1<<i))!=0){
+                    cntOnes++;
+                }
+                else{
+                    cntZero++;
+                }
+            }
+            if((cntOnes%3)!=0){
+                res=(res|(1<<i));
             }
         }
-        return ans;
+        return res;
     }
 };
