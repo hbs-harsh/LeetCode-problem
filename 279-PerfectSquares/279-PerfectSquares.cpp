a406c080b8abb96eb6192ee2e58d49d5c76185a2
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-04 16:40:48 +0530

    File updated at 7/4/2025, 4:40:48 PM

--- 279-PerfectSquares/279-PerfectSquares.cpp
+++ 279-PerfectSquares/279-PerfectSquares.cpp
@@ -1,19 +1,16 @@
-// Last updated: 7/4/2025, 4:21:42 PM
+// Last updated: 7/4/2025, 4:40:48 PM
 class Solution {
 public:
-int helper(vector<int>&dp,int n){
-    if(n==0) return 0;
-    int  ans=INT_MAX;
-    
-    if(dp[n]!=-1) return dp[n];
-    for(int i=1;i*i<=n;i++){
-            ans=min(ans,1+helper(dp,n-i*i));
-        }
-        return dp[n]=ans;
-}
     int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-        
-        return helper(dp,n);
+        vector<int>dp(n+1,INT_MAX);
+        if(n==0) return 0;
+        dp[0]=0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=min(dp[i],1+dp[i-j*j]);
+            }
+        }
+        return dp[n];
     }
-};  
+};
