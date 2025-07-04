885551bf2f18230a0efc39676edbb58bf5fb022c
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-04 15:51:03 +0530

    File updated at 7/4/2025, 3:51:03 PM

--- 377-CombinationSumIV/377-CombinationSumIV.cpp
+++ 377-CombinationSumIV/377-CombinationSumIV.cpp
@@ -1,21 +1,23 @@
-// Last updated: 7/4/2025, 3:19:43 PM
+// Last updated: 7/4/2025, 3:51:03 PM
 class Solution {
 public:
-int comb(int target,vector<int>& nums,vector<int>&dp){
-    int n=nums.size();
-    if(target==0) return 1;
-    if(target<0) return 0;
-    int cnt=0;
-    
-    for(int i=0;i<n;i++){
-        if(dp[target]!=-1) return dp[target];
-      cnt +=  comb(target-nums[i],nums,dp);
-    }
-    return dp[target]=cnt;
-}
     int combinationSum4(vector<int>& nums, int target) {
-        int n=nums.size();
-       vector<int>dp(target+1,-1);
-       return comb(target,nums,dp);
+        vector<unsigned long long> dp(target + 1,0);
+
+        // if(target<0) return 0;
+        // if(target==0) return 1;
+        dp[0] = 1;
+
+        for (int i = 1; i <=target; i++) {
+           for(int j=0;j<nums.size();j++){
+            int currNum= nums[j];
+            if(i-currNum>=0){
+                dp[i] += dp[i-currNum];
+            }
+
+           }
+        }
+
+       return dp[target]; 
     }
 };
