897dc973978c089f09267753a8573f755e16b5f2
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-07 14:53:41 +0530

    File updated at 7/7/2025, 2:53:40 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,17 +1,17 @@
-// Last updated: 7/7/2025, 2:50:40 PM
+// Last updated: 7/7/2025, 2:53:40 PM
 class Solution {
 public:
      int fun(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
-        if(i==s1.size()||j==s2.size()){
+        if(i<0||j<0){
             return 0;
         }
 
 
         if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j]){
-         return  dp[i][j]=1+fun(i+1,j+1,s1,s2,dp);
+         return  dp[i][j]=1+fun(i-1,j-1,s1,s2,dp);
         }
-         return dp[i][j]= max(fun(i+1,j,s1,s2,dp),fun(i,j+1,s1,s2,dp));
+         return dp[i][j]= max(fun(i-1,j,s1,s2,dp),fun(i,j-1,s1,s2,dp));
         
 
       
@@ -20,6 +20,6 @@ public:
         int m=text1.size();
         int n=text2.size();
         vector<vector<int>>dp(m,(vector<int>(n,-1)));
-        return fun(0,0,text1,text2,dp);
+        return fun(m-1,n-1,text1,text2,dp);
     }
 };
