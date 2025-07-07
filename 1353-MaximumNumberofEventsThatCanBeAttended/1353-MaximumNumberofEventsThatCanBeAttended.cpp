03862f2bc8da648f5f5416c07c833ded3786c473
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-08 00:00:45 +0530

    Create 1353-MaximumNumberofEventsThatCanBeAttended

---
+++ 1353-MaximumNumberofEventsThatCanBeAttended/1353-MaximumNumberofEventsThatCanBeAttended.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/8/2025, 12:00:45 AM
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        int n = events.size();
+        int maxDay = 0;
+        for (int i = 0; i < events.size(); i++) {
+            maxDay = max(maxDay, events[i][1]);
+        }
+        priority_queue<int, vector<int>, greater<>> pq;
+        sort(events.begin(), events.end());
+        int ans = 0;
+        for (int i = 0, j = 0; i <= maxDay; i++) {
+            while (j < n && events[j][0] <= i) {
+                pq.emplace(events[j][1]);
+                j++;
+            }
+            while (!pq.empty() && pq.top() < i) {
+                pq.pop();
+            }
+            if (!pq.empty()) {
+                pq.pop();
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
