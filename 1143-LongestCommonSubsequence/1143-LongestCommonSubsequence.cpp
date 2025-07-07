f55d4a5ad09f21eb3fbc5cc929b6abaec8430c01
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-07 15:16:01 +0530

    File updated at 7/7/2025, 3:16:00 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,25 +1,41 @@
-// Last updated: 7/7/2025, 2:53:40 PM
+// Last updated: 7/7/2025, 3:16:00 PM
 class Solution {
 public:
-     int fun(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
-        if(i<0||j<0){
-            return 0;
-        }
+    //  int fun(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+    //     if(i<0||j<0){
+    //         return 0;
+    //     }
 
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]){
-         return  dp[i][j]=1+fun(i-1,j-1,s1,s2,dp);
-        }
-         return dp[i][j]= max(fun(i-1,j,s1,s2,dp),fun(i,j-1,s1,s2,dp));
-        
-
-      
-     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s1[i]==s2[j]){
+    //      return  dp[i][j]=1+fun(i-1,j-1,s1,s2,dp);
+    //     }
+    //      return dp[i][j]= max(fun(i-1,j,s1,s2,dp),fun(i,j-1,s1,s2,dp));
+         
+    // }
     int longestCommonSubsequence(string text1, string text2) {
         int m=text1.size();
         int n=text2.size();
-        vector<vector<int>>dp(m,(vector<int>(n,-1)));
-        return fun(m-1,n-1,text1,text2,dp);
+        vector<vector<int>>dp(m+1,(vector<int>(n+1,0)));
+        return LCS(m,n,text1,text2,dp);
+    }
+
+
+    //tablu
+    int LCS(int m,int n,string &s1,string &s2,vector<vector<int>>&dp){
+
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
