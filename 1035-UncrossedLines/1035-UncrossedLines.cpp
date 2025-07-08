11b5b61098cebb374e32dfcdcb62b545598946db
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-08 15:03:14 +0530

    File updated at 7/8/2025, 3:03:13 PM

--- 1035-UncrossedLines/1035-UncrossedLines.cpp
+++ 1035-UncrossedLines/1035-UncrossedLines.cpp
@@ -1,24 +1,22 @@
-// Last updated: 7/8/2025, 2:55:15 PM
+// Last updated: 7/8/2025, 3:03:13 PM
 class Solution {
 public:
-int fun(int i,int j,vector<int>& nums1, vector<int>& nums2, vector<vector<int>>&dp){
-    if(i<0||j<0){
-        return 0;
-    }
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(nums1[i]==nums2[j]){
-        return dp[i][j]=1 + fun(i-1,j-1,nums1,nums2,dp);
-    }
-    else{
-        return dp[i][j]=max(fun(i-1,j,nums1,nums2,dp),fun(i,j-1,nums1,nums2,dp));
-    }
 
-}
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int m=nums1.size();
         int n=nums2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         
-        return fun(m-1,n-1,nums1,nums2,dp);
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(nums1[i-1]==nums2[j-1]){  //reason id dp[2][2] we are checkig [1,4] .ie 0,1
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
