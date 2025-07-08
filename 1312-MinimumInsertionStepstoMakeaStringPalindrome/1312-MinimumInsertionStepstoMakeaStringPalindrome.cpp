e9aefa9183a1a03900ffa67afd4e8b45b95c800e
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-08 16:40:57 +0530

    File updated at 7/8/2025, 4:40:56 PM

--- 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.cpp
+++ 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.cpp
@@ -1,32 +1,24 @@
-// Last updated: 5/17/2025, 1:11:22 AM
+// Last updated: 7/8/2025, 4:40:56 PM
 class Solution {
 public:
-int longestPalindromeSubseq(string s) {
-        string s1=s;
-       reverse(s1.begin(),s1.end());
-       int m=s.size();
-       int n=s1.size();
-     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-      
-      for(int i=0;i<=m;i++){
-        dp[i][0]=0;
-      }
-      for(int i=0;i<=n;i++){
-        dp[0][i]=0;
-      }
-      for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            if(s[i-1]==s1[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-            }
-            else{
-               dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-      }
-      return dp[m][n];
+int LPS(int i,int j,string&s,vector<vector<int>>&dp){
+    if(i == j){
+        return 1;
     }
+    if(i>j){
+        return 0;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==s[j]){
+        return dp[i][j]=2+LPS(i+1,j-1,s,dp);
+    }
+    else{
+        return dp[i][j]=max(LPS(i+1,j,s,dp),LPS(i,j-1,s,dp));
+    }
+}
     int minInsertions(string s) {
-        return s.size()-longestPalindromeSubseq(s);
+        int n=s.size();
+         vector<vector<int>> dp(n, vector<int>(n, -1));
+        return n-LPS(0,n-1,s,dp);
     }
 };
