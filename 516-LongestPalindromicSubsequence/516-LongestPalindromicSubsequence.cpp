b4deba9a9c0a50806a92bec43008529b9b92eebf
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-08 15:52:26 +0530

    File updated at 7/8/2025, 3:52:25 PM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
@@ -1,30 +1,28 @@
-// Last updated: 7/8/2025, 3:41:10 PM
+// Last updated: 7/8/2025, 3:52:25 PM
 class Solution {
 public:
-
+  
+int LPS(int i,int j,string&s,vector<vector<int>>&dp){
+    if(i == j){
+        return 1;
+    }
+    if(i>j){
+        return 0;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==s[j]){
+        return dp[i][j]=2+LPS(i+1,j-1,s,dp);
+    }
+    else{
+        return dp[i][j]=max(LPS(i+1,j,s,dp),LPS(i,j-1,s,dp));
+    }
+}
     int longestPalindromeSubseq(string s) {
-        string s1=s;
-       reverse(s1.begin(),s1.end());
-       int m=s.size();
-       int n=s1.size();
-     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-      
-      for(int i=0;i<=m;i++){
-        dp[i][0]=0;
-      }
-      for(int i=0;i<=n;i++){
-        dp[0][i]=0;
-      }
-      for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            if(s[i-1]==s1[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-            }
-            else{
-               dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-      }
-      return dp[m][n];
+        
+
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return LPS(0, n-1, s,dp);
     }
 };
