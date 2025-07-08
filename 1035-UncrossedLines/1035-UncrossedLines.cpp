ae5988db1181766b4c8ee8c07c2fba40a5468ad9
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-08 15:04:26 +0530

    File updated at 7/8/2025, 3:04:25 PM

--- 1035-UncrossedLines/1035-UncrossedLines.cpp
+++ 1035-UncrossedLines/1035-UncrossedLines.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/8/2025, 3:03:13 PM
+// Last updated: 7/8/2025, 3:04:25 PM
 class Solution {
 public:
 
@@ -6,7 +6,7 @@ public:
         int m=nums1.size();
         int n=nums2.size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        
+        //dp[i][j] means max UCL betwn num1 of size i and num2 of size j
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(nums1[i-1]==nums2[j-1]){  //reason id dp[2][2] we are checkig [1,4] .ie 0,1
