1274b86ac3eebbd9f2b2d0eb91d7942eaa09f264
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-09 15:38:36 +0530

    File updated at 7/9/2025, 3:38:35 PM

--- 72-EditDistance/72-EditDistance.cpp
+++ 72-EditDistance/72-EditDistance.cpp
@@ -1,31 +1,30 @@
-// Last updated: 7/9/2025, 3:17:37 PM
+// Last updated: 7/9/2025, 3:38:35 PM
 class Solution {
 public:
-int fun(int i,int j,string&s1,string &s2,vector<vector<int>>&dp){
-    if(i<0){
-        return j+1;
-    }
-    if(j<0){
-        return i+1;
-
-    }
-    if(dp[i][j]!=-1) return dp[i][j];
-    
-    if(s1[i]==s2[j]){
-        return dp[i][j]=fun(i-1,j-1,s1,s2,dp);
-    }
-    else{
-        int insert = 1+fun(i,j-1,s1,s2,dp);
-        int del= 1+ fun(i-1,j,s1,s2,dp);
-        int replace=1+fun(i-1,j-1,s1,s2,dp);
-        return dp[i][j]=min({insert, del, replace});
-    }
-}
     int minDistance(string word1, string word2) {
-        
-        int m=word1.size();
-        int n=word2.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(m-1,n-1,word1,word2,dp);
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Base cases
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+
+        // Tabulation
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min({
+                        dp[i][j - 1],     // Insert
+                        dp[i - 1][j],     // Delete
+                        dp[i - 1][j - 1]  // Replace
+                    });
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
