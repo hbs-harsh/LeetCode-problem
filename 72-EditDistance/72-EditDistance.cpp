7ddd09edbf8467856825ab778d9de939294a9579
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-09 15:17:37 +0530

    File updated at 7/9/2025, 3:17:37 PM

--- 72-EditDistance/72-EditDistance.cpp
+++ 72-EditDistance/72-EditDistance.cpp
@@ -1,23 +1,23 @@
-// Last updated: 7/9/2025, 2:52:08 PM
+// Last updated: 7/9/2025, 3:17:37 PM
 class Solution {
 public:
 int fun(int i,int j,string&s1,string &s2,vector<vector<int>>&dp){
-    if(i==s1.size()){
-        return s2.size()-j;
+    if(i<0){
+        return j+1;
     }
-    if(j==s2.size()){
-        return s1.size()-i;
+    if(j<0){
+        return i+1;
 
     }
     if(dp[i][j]!=-1) return dp[i][j];
     
     if(s1[i]==s2[j]){
-        return dp[i][j]=fun(i+1,j+1,s1,s2,dp);
+        return dp[i][j]=fun(i-1,j-1,s1,s2,dp);
     }
     else{
-        int insert = 1+fun(i,j+1,s1,s2,dp);
-        int del= 1+ fun(i+1,j,s1,s2,dp);
-        int replace=1+fun(i+1,j+1,s1,s2,dp);
+        int insert = 1+fun(i,j-1,s1,s2,dp);
+        int del= 1+ fun(i-1,j,s1,s2,dp);
+        int replace=1+fun(i-1,j-1,s1,s2,dp);
         return dp[i][j]=min({insert, del, replace});
     }
 }
@@ -26,6 +26,6 @@ int fun(int i,int j,string&s1,string &s2,vector<vector<int>>&dp){
         int m=word1.size();
         int n=word2.size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(0,0,word1,word2,dp);
+        return fun(m-1,n-1,word1,word2,dp);
     }
 };
