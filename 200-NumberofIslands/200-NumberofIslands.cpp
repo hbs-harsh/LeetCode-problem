35db3cc18e79f51591d33c510a077d79907a24ce
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-13 23:33:30 +0530

    File updated at 7/13/2025, 11:33:31 PM

--- 200-NumberofIslands/200-NumberofIslands.cpp
+++ 200-NumberofIslands/200-NumberofIslands.cpp
@@ -1,25 +1,50 @@
-// Last updated: 6/6/2025, 2:33:16 AM
+// Last updated: 7/13/2025, 11:33:31 PM
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid) {
+    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vis[row][col] = 1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
 
-        // 4 directions: up, down, left, right
-        int drow[] = {-1, 1, 0, 0};
-        int dcol[] = {0, 0, -1, 1};
+        while(!q.empty()){
+            int rrow=q.front().first;
+            int ccol=q.front().second;
+            q.pop();
 
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
+            int drow[] = {-1, 0, 1, 0};  // up, right, down, lef
+            int dcol[] = {0, 1, 0, -1};
+            for(int i=-0;i<4;i++){
+              
+                    int nrow=rrow+drow[i];
+                    int ncol=ccol+dcol[i];
 
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                grid[nrow][ncol] == '1' && !vis[nrow][ncol]) {
-                dfs(nrow, ncol, vis, grid);
+                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&
+                    !vis[nrow][ncol]&& grid[nrow][ncol]=='1'){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
+
+                
             }
         }
-    }
+
+     }
+        // 4 directions: up, down, left, right
+        // int drow[] = {-1, 1, 0, 0};
+        // int dcol[] = {0, 0, -1, 1};
+
+        // for (int i = 0; i < 4; i++) {
+        //     int nrow = row + drow[i];
+        //     int ncol = col + dcol[i];
+
+        //     if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+        //         grid[nrow][ncol] == '1' && !vis[nrow][ncol]) {
+        //         dfs(nrow, ncol, vis, grid);
+        //     }
+        // }
+    // }
 
 public:
     int numIslands(vector<vector<char>>& grid) {
@@ -32,7 +57,7 @@ public:
             for (int col = 0; col < m; col++) {
                 if (!vis[row][col] && grid[row][col] == '1') {
                     cnt++;
-                    dfs(row, col, vis, grid);
+                    bfs(row, col, vis, grid);
                 }
             }
         }
