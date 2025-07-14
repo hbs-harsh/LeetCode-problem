2a9f460880c5d1f445a606b98d467ea077305434
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-14 14:38:20 +0530

    File updated at 7/14/2025, 2:38:20 PM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,26 +1,39 @@
-// Last updated: 7/14/2025, 2:32:37 PM
+// Last updated: 7/14/2025, 2:38:20 PM
 class Solution {
-    private:
-    int findPath(int row,int col,int m,int n,vector<vector<int>>&dp){
+    // private:
+    // int findPath(int row,int col,int m,int n,vector<vector<int>>&dp){
 
-        if(row==m-1 || col==n-1){
-            return 1;
-        }
-        if(row==m||col==n){
-            return 0;
-        }
-        if(dp[row][col]!=-1) return dp[row][col];
-        int right=findPath(row,col+1,m,n,dp);
+    //     if(row==m-1 || col==n-1){
+    //         return 1;
+    //     }
+    //     if(row==m||col==n){
+    //         return 0;
+    //     }
+    //     if(dp[row][col]!=-1) return dp[row][col];
+    //     int right=findPath(row,col+1,m,n,dp);
 
-        int down=findPath(row+1,col,m,n,dp);
+    //     int down=findPath(row+1,col,m,n,dp);
 
-        return dp[row][col]=(right+down);
-    }
+    //     return dp[row][col]=(right+down);
+    // }
 
 public:
     int uniquePaths(int m, int n) {
 
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return findPath(0,0,m,n,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+        dp[m-1][n-1]=1;
+        for(int row=m-1;row>=0;row--){
+            for(int col=n-1;col>=0;col--){
+
+                if(row==m-1&&col==n-1) continue;
+                int right=dp[row][col+1];
+                int left=dp[row+1][col];
+
+                dp[row][col]=right+left;
+            }
+        }
+
+        return dp[0][0];
     }
 };
