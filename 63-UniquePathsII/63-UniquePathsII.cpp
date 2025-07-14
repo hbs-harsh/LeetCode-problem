46409533412090a01d96405caf945dd355421d6e
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-14 15:41:01 +0530

    File updated at 7/14/2025, 3:41:02 PM

--- 63-UniquePathsII/63-UniquePathsII.cpp
+++ 63-UniquePathsII/63-UniquePathsII.cpp
@@ -1,35 +1,45 @@
-// Last updated: 7/14/2025, 3:21:20 PM
+// Last updated: 7/14/2025, 3:41:02 PM
 class Solution {
 public:
-int findPath(int row,int col,vector<vector<int>>&grid,
-vector<vector<int>>&dp){
-        if(row<0|| col<0 ||grid[row][col]==1){
-            return 0;
-        }
-        if(row==0 && col==0){
-            return 1;
-        }
 
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-     
-        int left=findPath(row,col-1,grid,dp);
-
-        int up=findPath(row-1,col,grid,dp);
-
-       
-        
-
-        return dp[row][col]=left+up;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1){
             return 0;
         }
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return findPath(m-1,n-1,obstacleGrid,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+      
+        dp[0][0]=1;
+        for(int i=1;i<m;i++){
+            if(obstacleGrid[i][0]==1){
+                dp[i][0]=0;
+            }
+            else{
+                dp[i][0]=dp[i-1][0];
+            }
+        }
+        for(int j=1;j<n;j++){
+            if(obstacleGrid[0][j]==1){
+                dp[0][j]=0;
+            }
+            else{
+                dp[0][j]=dp[0][j-1];
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }
+                else{
+                int way1=dp[i-1][j];
+                int way2=dp[i][j-1];
+                dp[i][j]=way1+way2;
+            }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
