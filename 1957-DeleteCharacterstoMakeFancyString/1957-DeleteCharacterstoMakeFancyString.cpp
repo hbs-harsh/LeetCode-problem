08fdeacb6c5f1e3f5636ad4ccd7a5f2c1737b696
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-07-22 00:38:40 +0530

    Create 1957-DeleteCharacterstoMakeFancyString

---
+++ 1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/22/2025, 12:38:40 AM
+class Solution {
+public:
+    string makeFancyString(string s) {
+        char prev = s[0];
+        int frequency = 1;
+        string ans = "";
+        ans.push_back(s[0]);
+
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == prev) {
+                frequency++;
+            } else {
+                prev = s[i];
+                frequency = 1;
+            }
+
+            if (frequency < 3) ans.push_back(s[i]);
+        }
+
+        return ans;
+    }
+};
