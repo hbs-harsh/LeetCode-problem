3f9158791baa24000fabbb1e044024ccec72aeaa
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-09-15 22:53:04 +0530

    File updated at 9/15/2025, 10:53:03 PM

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,7 +1,34 @@
-// Last updated: 9/15/2025, 10:42:17 PM
+// Last updated: 9/15/2025, 10:53:03 PM
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        
+        int cntZero=0;
+        int cntOne=0;
+        int cntTwo=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                cntZero++;
+            }
+            else if(nums[i]==1){
+                cntOne++;
+            }
+            else{
+                cntTwo++;
+            }
+
+        }
+        for(int i=0;i<cntZero;i++){
+            nums[i]=0;
+        }
+        for(int i=cntZero;i<cntZero+cntOne;i++){
+            nums[i]=1;
+        }
+        for(int i=cntZero+cntOne;i<n;i++){
+            nums[i]=2;
+        }
+       
     }
 };
