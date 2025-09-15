ba666245b68f0ec3bb9f4e046d031e72192d5dd3
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-09-16 01:31:58 +0530

    Create 1304-FindNUniqueIntegersSumuptoZero

---
+++ 1304-FindNUniqueIntegersSumuptoZero/1304-FindNUniqueIntegersSumuptoZero.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/16/2025, 1:31:57 AM
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        for (int i = 1; i <= n / 2; ++i) {
+            ans.push_back(i);
+            ans.push_back(-i);
+        }
+        if (n % 2 == 1) {
+            ans.push_back(0);
+        }
+        return ans;
+    }
+};
