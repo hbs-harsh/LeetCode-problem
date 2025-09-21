0bd9746779d7a6478918c3a3d4ce23f61a5bb985
Harsh Bardhan Saxena <120386263+hbs-harsh@users.noreply.github.com>
2025-09-21 22:43:38 +0530

    File updated at 9/21/2025, 10:43:37 PM

--- 268-MissingNumber/268-MissingNumber.cpp
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -1,15 +1,17 @@
-// Last updated: 9/21/2025, 10:32:10 PM
+// Last updated: 9/21/2025, 10:43:37 PM
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i){
-            return i;
-            }
-        }
-        return n;
+        // sort(nums.begin(),nums.end());
+       int total= n*(n+1)/2;
+
+       int sum=0;
+
+       for(int x: nums){
+        sum += x;
+       }
+       return total-sum;
     }
 };
